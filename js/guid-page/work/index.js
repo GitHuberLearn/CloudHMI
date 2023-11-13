fbfcff10fb8ba90f0dc6c1c00a23ad512cf6fc52
yangke <yangke@shxseer.com>
2023-11-13 18:05:35 +0800

    净现率

--- js/guid-page/work/index.js
+++ js/guid-page/work/index.js
@@ -6,13 +6,15 @@ let { getDateN } = await import(
   `${cube.gatewayURL_module}js/product/mockData.js`
 );
 
+let data = {
+  value: 6,
+};
 //选择项目文件右击打开
 $(function () {
   const time = {
     time: "2023-08-28",
     num: 0,
   };
-  //大→小
   const list = (time) => {
     const months24 = [24, 12, 6, 3, 24, 12, 6, 3, 24, 12, 6, 3, 24, 12, 6, 3];
     let month24Date = [];
@@ -53,7 +55,6 @@ $(function () {
 
     const chartList = [
       {
-        xAxisIndex: 0,
         name: "2年",
         type: "line",
         smooth: true,
@@ -76,7 +77,6 @@ $(function () {
         date: month24Date,
       },
       {
-        xAxisIndex: 1,
         name: "1年",
         type: "line",
         smooth: true,
@@ -99,7 +99,6 @@ $(function () {
         date: month12Date,
       },
       {
-        xAxisIndex: 2,
         name: "半年",
         type: "line",
         smooth: true,
@@ -122,7 +121,6 @@ $(function () {
         date: month06Date,
       },
       {
-        xAxisIndex: 3,
         name: "3个月",
         type: "line",
         smooth: true,
@@ -145,40 +143,9 @@ $(function () {
         date: month03Date,
       },
     ];
-    const chartList1 = [
-      {
-        xAxisIndex: 0,
-        name: "2年",
-        type: "line",
-        smooth: true,
-        lineStyle: { color: "#FDAD26" },
-        itemStyle: { color: "#FDAD26" },
-        areaStyle: {
-          opacity: 0.5,
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: "#FDAD26",
-            },
-            {
-              offset: 0.8,
-              color: "#322C5B",
-            },
-          ]),
-        },
-        data: months24,
-        date: month24Date,
-      },
-    ];
-
     return chartList;
   };
-  const _config = option(list(time), "大→小");
-  const myChart = echarts.init(document.getElementById("charts"));
-  initEchart(myChart, _config);
-
-  //小→大
-  const listBig = (time, bg, index) => {
+  const listBig = (time, bg) => {
     const monthsBig03 = [
       3, 6, 12, 24, 3, 6, 12, 24, 3, 6, 12, 24, 3, 6, 12, 24,
     ];
@@ -223,10 +190,8 @@ $(function () {
       monthBig24Date.push(day);
     });
 
-    const xindex = index ? index : 0;
     const chartList = [
       {
-        xAxisIndex: xindex,
         name: "2年",
         type: "line",
         smooth: true,
@@ -249,7 +214,6 @@ $(function () {
         date: monthBig24Date,
       },
       {
-        xAxisIndex: 1 + xindex,
         name: "1年",
         type: "line",
         smooth: true,
@@ -272,7 +236,6 @@ $(function () {
         date: monthBig12Date,
       },
       {
-        xAxisIndex: 2 + xindex,
         name: "半年",
         type: "line",
         smooth: true,
@@ -295,7 +258,6 @@ $(function () {
         date: monthBig06Date,
       },
       {
-        xAxisIndex: 3 + xindex,
         name: "3个月",
         type: "line",
         smooth: true,
@@ -318,83 +280,55 @@ $(function () {
         date: monthBig03Date,
       },
     ];
-    const chartList1 = [
-      {
-        xAxisIndex: xindex,
-        name: "2年",
-        type: "line",
-        smooth: true,
-        lineStyle: { color: "#FDAD26" },
-        itemStyle: { color: "#FDAD26" },
-        areaStyle: {
-          opacity: 0.5,
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: "#FDAD26",
-            },
-            {
-              offset: 0.8,
-              color: "#322C5B",
-            },
-          ]),
-        },
-        data: monthsBig24,
-        date: monthBig24Date,
-      },
-    ];
     return chartList;
   };
-  const _configBig = option(listBig(time, true), "小→大");
-  const myChartBig = echarts.init(document.getElementById("chartsBig"));
-  initEchart(myChartBig, _configBig);
-
-  //组合
   const listGroup = (time) => {
     const group = list(time);
-    const groupBig = listBig(time, true, 4);
+    const groupBig = listBig(time, true);
     const chartList = [...group, ...groupBig];
     return chartList;
   };
-  const _confiGroup = option(listGroup(time), "组合");
-  const myChartGroup = echarts.init(document.getElementById("chartsGroup"));
-  initEchart(myChartGroup, _confiGroup);
+  const listChart = () => {
+    //大→小
+    const _config = option(list(time), "大→小");
+    const myChart = echarts.init(document.getElementById("charts"));
+    initEchart(myChart, _config);
+    //小→大
+    const _configBig = option(listBig(time, true), "小→大");
+    const myChartBig = echarts.init(document.getElementById("chartsBig"));
+    initEchart(myChartBig, _configBig);
+    //组合
+    const _confiGroup = option(listGroup(time), "组合");
+    const myChartGroup = echarts.init(document.getElementById("chartsGroup"));
+    initEchart(myChartGroup, _confiGroup);
+  };
+
+  listChart();
+  $("#clicks").on("change", function () {
+    const value = $(this).val();
+    data.value = value;
+    listChart();
+  });
 });
 
-function option(chartList, title) {
-  let xAxis = [];
-  let series = [];
+function option(chartList, title, value) {
+  let series = [],
+    yAxis = [];
   chartList.forEach((el, index) => {
-    const show = index === 0 ? true : false;
-    xAxis.push({
-      show,
-      type: "category",
-      boundaryGap: false,
-      axisLabel: {
-        color: "#fff",
-        // formatter: function (value) {
-        // 	return value.slice(5);
-        // },
-      },
-      data: el.date,
+    yAxis.push({
+      ...el,
+      xAxisIndex: 0,
+      data: [],
+    });
+    el.data.forEach((m, i) => {
+      yAxis[index].data.push([el.date[i], m]);
     });
   });
-
-  const { x, xy, y, xy1 } = optionxy(chartList);
-
-  xAxis.push({
-    show: false,
-    type: "category",
-    boundaryGap: false,
-    axisLabel: {
-      color: "#fff",
-    },
-    data: x,
-  });
+  const { x, xy, y, xy1, xyn } = optionxy(chartList);
+  const months = value ? value : 6;
   series.push(
-    ...chartList,
+    ...yAxis,
     {
-      xAxisIndex: title === "组合" ? 8 : 4,
       name: "净现率", //按照时间(重复率：斜率为0)
       type: "line",
       smooth: true,
@@ -416,7 +350,6 @@ function option(chartList, title) {
       data: xy,
     },
     {
-      xAxisIndex: title === "组合" ? 8 : 4,
       name: "净现值", //按照时间去重(重复率：净现率相差)
       type: "line",
       smooth: true,
@@ -438,26 +371,46 @@ function option(chartList, title) {
       data: y,
     },
     {
-      xAxisIndex: title === "组合" ? 8 : 4,
-      name: "跨度", //按照时间去重(重复率：净现率相差)
+      name: "全量跨度",
       type: "line",
       smooth: true,
-      lineStyle: { color: "#009885" },
-      itemStyle: { color: "#009885" },
+      lineStyle: { color: "#F508ED" },
+      itemStyle: { color: "#F508ED" },
       areaStyle: {
         opacity: 0.5,
         color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
           {
             offset: 0,
-            color: "#93C6C1",
+            color: "#D867D8",
           },
           {
             offset: 0.8,
-            color: "#2C5852",
+            color: "#9B409D",
           },
         ]),
       },
       data: xy1,
+    },
+    {
+      name: "跨度" + data.value + "个月",
+      type: "line",
+      smooth: true,
+      lineStyle: { color: "#F508ED" },
+      itemStyle: { color: "#F508ED" },
+      areaStyle: {
+        opacity: 0.5,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: "#D867D8",
+          },
+          {
+            offset: 0.8,
+            color: "#9B409D",
+          },
+        ]),
+      },
+      data: xyn,
     }
   );
   return {
@@ -476,17 +429,27 @@ function option(chartList, title) {
       confine: true,
     },
     legend: {
-      x: "right",
+      x: "center",
       textStyle: { color: "white" },
       padding: [28, 10, 0, 0],
     },
     grid: {
       top: "20%",
       left: "5%",
-      right: "3%",
+      right: "5%",
       bottom: "10%",
     },
-    xAxis,
+    xAxis: {
+      type: "category",
+      boundaryGap: false,
+      axisLabel: {
+        color: "#fff",
+        // formatter: function (value) {
+        // 	return value.slice(5);
+        // },
+      },
+      data: x,
+    },
     yAxis: [
       {
         type: "value",
@@ -516,8 +479,6 @@ function optionxy(chartList) {
       });
     });
   });
-
-  //从小到大时间
   const data = sortMin(xAxisList);
   let xy = [],
     obj = [];
@@ -528,11 +489,11 @@ function optionxy(chartList) {
       y: el.y,
     });
   });
-  const { x, y, xy1 } = getMonthMax(obj);
-  return { xy, x, y, xy1 };
+  const { x, y, xy1, xyn } = getMonthMax(obj);
+  return { xy, x, y, xy1, xyn };
 }
 
-//排序(从小到大)
+//排序(时间从小到大)
 function sortMin(xAxisList) {
   let list = xAxisList.concat([]); //防止改变原数据
   list.sort(function (a, b) {
@@ -542,44 +503,65 @@ function sortMin(xAxisList) {
   });
   return list;
 }
-//去重
+
 function getMonthMax(obj) {
+  //去重
   let map = new Map();
   const newArr = obj.filter((v) => !map.has(v.x) && map.set(v.x, v));
   let x = [],
     y = [];
-
-  let mm = [];
-
-  newArr.forEach((el, index) => {
-    let txl = 0,
-      tafxl = 0;
-    if (index < newArr.length - 1) {
-      const xl = el.x.substring(0, 7);
-      txl = Date.parse(xl.replace(/-/g, "/"));
-      const afxl = newArr[index + 1].x.substring(0, 7);
-      tafxl = Date.parse(afxl.replace(/-/g, "/"));
-    }
+  newArr.forEach((el) => {
     x.push(el.x);
     y.push(el.y);
-    mm.push({
-      x: el.x,
-      y: el.y,
-      t: tafxl - txl,
-    });
   });
-  console.log(mm);
-  //从小到大
-  mm.sort(function (a, b) {
-    return a.t - b.t;
+  //月跨度率（31）
+  const mm = obj.map((el, index) => {
+    const spread = teve(el, index, obj);
+    return spread;
   });
-  //获取最大
-  const maxs = mm.filter((item) => item.t === 7948800000);
+  // 跨度从小到大
+  const list = mm.concat([]); //防止改变原数据
+  list.sort(function (a, b) {
+    return a.n - b.n;
+  });
+  let xy = [],
+    xyn = [],
+    n = [];
+  list.forEach((el) => {
+    n.push(el.n);
+    xy.push([el.x, el.n]);
+    if (el.n === Number(data.value)) {
+      xyn.push([el.x, el.y]);
+    }
+  });
+  //let arr = Array.from(new Set(n));
+  //console.log(arr);
+  return { x, y, xy1: xy, xyn };
+}
 
-  let xy = [];
-  mm.forEach((el) => {
-    xy.push([el.x, el.y]);
-  });
-
-  return { x, y, xy1: xy };
+// 月跨度差（31）
+function teve(el, index, obj) {
+  let txl = 0,
+    tafxl = 0,
+    l = 0;
+  if (index < obj.length - 1) {
+    const xl = el.x.substring(0, 7);
+    txl = Date.parse(xl.replace(/-/g, "/"));
+    const afxl = obj[index + 1].x.substring(0, 7);
+    tafxl = Date.parse(afxl.replace(/-/g, "/"));
+    const xln = xl.split("-")[1];
+    const afxln = afxl.split("-")[1];
+    if (xln > afxln) {
+      l = 12 - xln + Number(afxln);
+    } else {
+      l = afxln - xln;
+    }
+  }
+  const monthdiffer = Math.trunc((tafxl - txl) / (1000 * 60 * 60 * 24 * 31));
+  return {
+    x: el.x,
+    y: el.y,
+    t: monthdiffer,
+    n: l,
+  };
 }
